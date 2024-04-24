f1fcf743621eaabb86e776507253087a561c94ed
Melchior Oudemans <melchior.oudemans@gmail.com>
2024-04-24 09:18:16 +0200

    Work on P7 Benchmark

--- benchmarks/src/test/java/P7TransportationTest.java
+++ benchmarks/src/test/java/P7TransportationTest.java
@@ -15,8 +15,31 @@ public class P7TransportationTest {
 
         for (int i = start_test_id; i <= end_test_id; i++) {
             try {
-                String fileName = "src/main/resources/P7/MANUAL/test" + i + ".ser";
-                MyGraph g = MyGraph.readGraphFromFile(fileName);
+//                String fileName = "src/main/resources/P7/MANUAL/test" + i + ".ser";
+                String fileName = "src/main/resources/P7/MANUAL/test" + i + ".json";
+                MyGraph g = MyGraph.readGraphFromJSON(fileName);
+                analysis.run(g);
+            } catch (Exception e) {
+                System.err.println("Caught exception: " + e.getMessage());
+            }
+        }
+
+
+        assertTrue("test", true);
+    }
+
+    @org.junit.Test
+    public void testPGMARK() {
+        P7Logic analysis = new P7Logic();
+
+        int start_test_id = 1;
+        int end_test_id = 10;
+
+        for (int i = start_test_id; i <= end_test_id; i++) {
+            try {
+//                String fileName = "src/main/resources/P7/MANUAL/test" + i + ".ser";
+                String fileName = "src/main/resources/P7/PGMark-FIXED/test" + i + ".json";
+                MyGraph g = MyGraph.readGraphFromJSON(fileName);
                 analysis.run(g);
             } catch (Exception e) {
                 System.err.println("Caught exception: " + e.getMessage());
