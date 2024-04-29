cb5827187236104a70ef57a9f1bcc71a9c6f6756
Melchior Oudemans <melchior.oudemans@gmail.com>
2024-04-29 09:51:44 +0200

    Initialised P10

---
+++ benchmarks/src/main/java/P10Pangenomic/P10Logic.java
@@ -0,0 +1,19 @@
+package P10Transportation;
+
+
+
+import tudcomponents.Edge;
+import tudcomponents.MyGraph;
+import tudcomponents.Node;
+
+import java.util.ArrayList;
+
+public class P10Logic {
+    MyGraph my_g;
+
+    public void run(MyGraph g) {
+
+    }
+
+
+}
