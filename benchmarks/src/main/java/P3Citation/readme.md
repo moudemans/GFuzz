5f9e84bc512d9751b8fc4cf6df7397d652db921e
Melchior Oudemans <melchior.oudemans@gmail.com>
2024-05-07 15:53:25 +0200

    More benchmark fixes in naming

--- benchmarks/src/main/java/P3Citation/readme.md
+++ benchmarks/src/main/java/P3Citation/readme.md
@@ -7,7 +7,7 @@
 **Motivation** \
 The below program has been collected from the following github [repository](https://github.com/Citegraph/citegraph/tree/main).
 
-The repo shows graph data being written to an object in memory: [](https://github.com/Citegraph/citegraph/blob/main/backend/src/main/java/io/citegraph/app/GraphController.java).
+The repo shows graph data being written to an object in memory: [here](https://github.com/Citegraph/citegraph/blob/main/backend/src/main/java/io/citegraph/app/GraphController.java).
 
 
 ![Sample code](../../../../../docs/media/benchmarks/P4/full_schema.png)
---
+++ benchmarks/src/test/java/P3Citation.java
@@ -0,0 +1,56 @@
+
+import P3Citation.P3Logic;
+import tudcomponents.MyGraph;
+import util.Util;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
+public class P3Citation {
+
+    String input_path ="src/main/resources/P3/";
+    boolean run_json = true;
+    boolean run_ser = true;
+
+    @org.junit.Test
+    public void testManual() {
+        String path = input_path + "MANUAL/";
+        testFilesInDir(path);
+    }
+    public void testFilesInDir(String path) {
+        P3Logic analysis = new P3Logic();
+
+        if (!Util.dirExists(path)) {
+            System.err.println("Input directory not found: " + path);
+            System.out.println("Working Directory = " + System.getProperty("user.dir"));
+
+            return;
+        }
+
+        File input_dir = new File(path);
+        File[] listOfFiles = input_dir.listFiles();
+        for (File f :
+                listOfFiles) {
+            MyGraph g;
+            if (run_json && f.getPath().contains("json")) {
+                g = MyGraph.readGraphFromJSON(f.getPath());
+            } else if (run_ser && f.getPath().contains("ser")) {
+                g = MyGraph.readGraphFromFile(f.getPath());
+            } else {
+                continue;
+            }
+
+
+            try {
+                analysis.run(g);
+            } catch (Exception e) {
+                System.err.println("Caught exception: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+        assertTrue("test", true);
+    }
+
+}
+
